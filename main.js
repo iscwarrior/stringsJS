9fbad6da298c44d4dd2566674f963650048845b8
iscwarrior <dani.fguerrero@gmail.com>
2023-04-26 13:14:36 -0600

    clean code

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-/*- se necesita las iniciales de cualquier nombre completo:
+/*  Se necesita las iniciales de cualquier nombre completo:
     nombre, apellido materno, apellido paterno
-        Israel Salinas Martines -> I. S. M.
-        Jorge del Rio Hernandez -> D.D.H
+    Ejemplos: 
+        - Israel Salinas Martines -> I. S. M.
+        - Jorge del Rio Hernandez -> D.D.H
 */
-
 let completeName = {
     name: "Daniel",
     lastName1: "Flores",
@@ -11,14 +11,11 @@ let completeName = {
 }
 
 const mayusLetter = nombre => {
-    let name1 = nombre.name.substring(0,1);
-    let lastname_1 = nombre.lastName1.substring(0,1);
-    let lastname_2 = nombre.lastName2.substring(0,1);
-    let lastname_3 = nombre.name.concat(" "+nombre.lastName1).concat(" "+nombre.lastName2);
-    // console.log(name1)
-    // console.log(lastname_1)
-    // console.log(lastname_2)
-    // console.log(lastname_3)
+    let name1 = nombre.name.substring(0, 1);
+    let lastname_1 = nombre.lastName1.substring(0, 1);
+    let lastname_2 = nombre.lastName2.substring(0, 1);
+    let lastname_3 = nombre.name.concat(" " + nombre.lastName1).concat(" " + nombre.lastName2);
+    // console.log(name1) // console.log(lastname_1) // console.log(lastname_2) // console.log(lastname_3)
     let complemeteName2 = name1.concat(". ").concat(lastname_1).concat(". ").concat(lastname_2).concat(".");
     let result = console.log(complemeteName2)
     return result, lastname_3
@@ -26,9 +23,5 @@ const mayusLetter = nombre => {
 
 console.log(mayusLetter(completeName))
 
-
-let [primerNombre, apellidoP, apellidoM ] = lastname_3.split(' ');
-console.log(primerNombre, apellidoP, apellidoM);
-console.log(apellidoP) 
 
 
